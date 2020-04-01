33d71405cd97ea40e7f7de962aa57479d6f91497
ducthai <dbom.1006@gmail.com>
2020-04-01 22:41:33 +0700

    Update config URL

--- config/config.js
+++ config/config.js
@@ -72,7 +72,7 @@ export default {
   define: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION:
       ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION || '', // preview.pro.ant.design only do not use in your production ; preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
-    API_URL_DEFAULT: API_URL || 'https://api-container.herokuapp.com/',
+    API_URL_DEFAULT: 'https://api-container.herokuapp.com/',
     SENTRY_DSN: process.env.SENTRY_DSN,
   },
   ignoreMomentLocale: true,
